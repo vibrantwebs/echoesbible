32592fd43e6f03866fcf1086ba0bbdb7689df1cb
Andrew Musholt <andrew@vibrantwebs.com>
2026-03-14 10:26:32 -0500

    saving before 7.4 downgrade

--- docs/structure.sql
+++ docs/structure.sql
@@ -1,3 +1,13 @@
+/** todo:
+ * At a glance, these seem to be "markup heavy" and not as "row heavy" or "relationship heavy"
+ * As in, if we only have 100 or so of these, there's not necessarily a great reason to store them in the database.
+ * Searching may be an exception.
+ *
+ * todo:
+ * When we finalize this structure, we will need data entry into a spreadsheet that I can mass-import.
+ * Maybe use AI?
+ */
+
 CREATE DATABASE moose_echoesbible;
 
 CREATE TABLE user (
@@ -7,5 +17,35 @@ CREATE TABLE user (
     first_name VARCHAR(100) NOT NULL,
     last_Name VARCHAR(100) NOT NULL,
     about_me TEXT NOT NULL,
+    created_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE echo_group (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL DEFAULT 'Untitled',
+    commentary TEXT NOT NULL,
+    created_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE echo_sub_group (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title_left VARCHAR(255) NOT NULL DEFAULT 'Untitled',
+    title_right VARCHAR(255) NOT NULL DEFAULT 'Untitled',
+    body_left TEXT NOT NULL,
+    body_right TEXT NOT NULL,
+    commentary TEXT NOT NULL,
+    created_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE echo (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title_left VARCHAR(255) NOT NULL DEFAULT 'Untitled',
+    title_left VARCHAR(255) NOT NULL DEFAULT 'Untitled',
+    fragment_left TEXT NOT NULL,
+    fragment_right TEXT NOT NULL,
+    translation_hebrew VARCHAR(255) NOT NULL DEFAULT '', /** todo: each translation has 3 parts..."symbols", native language, English */
+    translation_lxx VARCHAR(255) NOT NULL DEFAULT '',
+    translation_greek VARCHAR(255) NOT NULL DEFAULT '',
+    commentary TEXT NOT NULL,
     created_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
